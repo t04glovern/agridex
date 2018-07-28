fc533b616bc0350118c62b279625aef4de99ae03
Rico A. Beti <rico.beti@silentbyte.com>
2018-07-28 18:25:07 +0800

    Add tab bar for sheep details

--- lib/ui/sheep_details/footer/details_footer.dart
+++ lib/ui/sheep_details/footer/details_footer.dart
@@ -1,6 +1,94 @@
 import 'package:agridex/models/sheep.dart';
-import 'package:agridex/ui/sheep_details/footer/showcase_details.dart';
 import 'package:flutter/material.dart';
+
+class SheepDetail extends StatelessWidget {
+  final Sheep sheep;
+
+  SheepDetail(this.sheep);
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+    var fieldCaption = textTheme.title.copyWith(color: Colors.white);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 10.0),
+                child: Text('EID', style: fieldCaption),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius:
+                      BorderRadius.all(Radius.elliptical(50.0, 100.0)),
+                  gradient: LinearGradient(
+                    begin: FractionalOffset.centerRight,
+                    end: FractionalOffset.bottomLeft,
+                    colors: [
+                      Colors.white,
+                      Colors.white,
+                    ],
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+                  child: Text(
+                    this.sheep.eid,
+                    style: textTheme.body1
+                        .copyWith(color: Colors.black, fontSize: 30.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+
+//    return new Center(
+//      child: new Text(
+//        "EID: " +
+//            sheep.eid +
+//            "\nBirth: " +
+//            sheep.birth.toString() +
+//            "\nGender: " +
+//            sheep.sex,
+//        textAlign: TextAlign.center,
+//        style: textTheme.subhead.copyWith(color: Colors.white),
+//      ),
+//    );
+  }
+}
+
+class SheepPedigree extends StatelessWidget {
+  final Sheep sheep;
+
+  SheepPedigree(this.sheep);
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+
+    return new Center(
+      child: new Text(
+        "EID: " +
+            sheep.eid +
+            "\nBirth: " +
+            sheep.birth.toString() +
+            "\nGender: " +
+            sheep.sex,
+        textAlign: TextAlign.center,
+        style: textTheme.subhead.copyWith(color: Colors.white),
+      ),
+    );
+  }
+}
 
 class SheepShowcase extends StatefulWidget {
   final Sheep sheep;
@@ -22,9 +110,11 @@ class _SheepShowcaseState extends State<SheepShowcase>
     super.initState();
     _tabs = [
       new Tab(text: 'Details'),
+      new Tab(text: 'Pedigree'),
     ];
     _pages = [
-      new DetailsShowcase(widget.sheep),
+      new SheepDetail(widget.sheep),
+      new SheepPedigree(widget.sheep),
     ];
     _controller = new TabController(
       length: _tabs.length,
@@ -39,6 +129,7 @@ class _SheepShowcaseState extends State<SheepShowcase>
       child: new Column(
         children: [
           new TabBar(
+            labelStyle: Theme.of(context).textTheme.title,
             controller: _controller,
             tabs: _tabs,
             indicatorColor: Colors.white,
